c8d312eaad4a6c4f370e6f148068407fe79d594d
Jimmy Jemimah <jjimmybadugu@gmail.com>
2026-07-14 15:55:56 +0530

    Added LBH1 Day 3 Java programs

---
+++ LBH1/day3/IsOdd.java
@@ -0,0 +1,15 @@
+public class IsOdd {
+
+    public static int isOdd(int num) {
+        if (num % 2 != 0)
+            return 2;
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOdd(7));
+        System.out.println(isOdd(8));
+        System.out.println(isOdd(0));
+    }
+}
